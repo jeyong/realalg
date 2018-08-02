249fe6cc071170d7ef6a8ded7267b26dc83c9eb0
Jeyong Shin <jeyong@gmail.com>
2018-08-02 12:40:20 +0900

    Create graph_bft.py

---
+++ graph_bft.py
@@ -0,0 +1,27 @@
+
+class Graph:
+    def __init__(self):
+        self.vertexs={"S":["A", "B"], "A":["F"], "B": ["A", "F"], "F": None}
+
+    def __str__(self):
+        print(str(self.vertexs))
+
+    def BFT(self, start):
+        if list(self.vertexs.keys()).count(start) == 0 :
+            return 
+        q = []
+        visited = []
+        q.append(start)
+        while len(q) > 0 :
+            v = q[0]
+            q = q[1:]
+            if v in visited:
+                continue 
+            print (v, "-> ")
+            visited.append(v)
+            if self.vertexs[v] :
+                q.extend(self.vertexs[v])
+
+if __name__ == "__main__":
+    g = Graph()
+    g.BFT("S")
